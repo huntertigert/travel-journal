ad6f5d10fa19c66b05911a7d66e09cf12f328b43
huntertigert <huntermtigert@gmail.com>
2026-07-06 11:57:26 -0500

    Initial commit

---
+++ data.js
@@ -0,0 +1,38 @@
+export default [
+    {
+        id: 1,
+        img: {
+            src: "https://pcmedia.ign.com/pc/image/article/572/572070/world-of-warcraft-20041209010920394-001.jpg?width=1280&height=720&fit=bounds&format=jpg&auto=webp&quality=80",
+            alt: "Stormwind City"
+        },
+        title: "Stormwind City",
+        country: "Eastern Kingdoms",
+        googleMapsLink: "https://wowpedia.fandom.com/wiki/Stormwind_City",
+        dates: "12 Jan, 2021 - 24 Jan, 2021",
+        text: "Stormwind is the capital city of the Alliance and one of the most important cities in Azeroth."
+    },
+    {
+        id: 2,
+        img: {
+            src: "https://oyster.ignimgs.com/mediawiki/apis.ign.com/mass-effect/7/79/Expose_Saren_slice1.png?width=814&format=jpg&auto=webp&quality=80",
+            alt: "The Citadel"
+        },
+        title: "The Citadel",
+        country: "Space",
+        googleMapsLink: "https://masseffect.fandom.com/wiki/Citadel",
+        dates: "27 May, 2021 - 8 Jun, 2021",
+        text: "The Citadel is a massive space station and the headquarters of the Council of the Citadel, located in the Milky Way galaxy."
+    },
+    {
+        id: 3,
+        img: {
+            src: "https://oyster.ignimgs.com/mediawiki/apis.ign.com/elden-ring/a/a1/20220219134149_1.jpg?width=814&format=jpg&auto=webp&quality=80",
+            alt: "Geirangerfjord"
+        },
+        title: "Stormveil Castle",
+        country: "The Lands Between",
+        googleMapsLink: "https://eldenring.wiki.fextralife.com/Interactive+Map?id=903&code=mapA",
+        dates: "01 Oct, 2021 - 18 Nov, 2021",
+        text: "Stormveil Castle is a massive fortress located in the Lands Between, home to the demigod Godrick the Grafted and one of the main locations in Elden Ring."
+    },
+]
